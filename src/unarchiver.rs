0d1c8d6cc98435d79ff4ccd59b98ccb0c82b747c
sourcepirate <plasmashadowx@gmail.com>
2017-10-10 12:35:55 +0530

    Removed dead code"

--- src/unarchiver.rs
+++ src/unarchiver.rs
@@ -10,6 +10,7 @@ use std::collections::HashMap;
 use executor::join;
 
 
+
 pub struct Unarchiver{
     pub path: String
 }
@@ -19,35 +20,6 @@ impl Unarchiver{
         Unarchiver{
             path: path.to_string()
         }
-    }
-
-    pub fn unarchive_all(&self, dest: String) -> Result<Vec<String>, Error>{
-        let fp = Path::new(&self.path[..]);
-        let mut readers : Vec<String> = vec![];
-        let mut files = fp.read_dir().unwrap();
-        for file in files{
-            let file_path = file.unwrap().path();
-            let file_name = file_path.to_str().unwrap();
-            if file_name.contains(".tar"){
-                let opened_file : File= File::open(file_name).unwrap();
-                let decompressed = GzDecoder::new(opened_file).unwrap();
-                let mut archive = Archive::new(decompressed);
-                let mut first: bool = true;
-                for file in archive.entries().unwrap(){
-                    let mut file = file.unwrap();
-                    let val = file.header().path().unwrap().to_str().unwrap().to_string();
-                    let buf = join(&dest[..], &val[..]).to_str().unwrap().to_string();
-                    if first {
-                        readers.push(buf);
-                        first = false;
-                    }
-
-                    file.unpack_in(&dest[..]).unwrap();
-
-                }
-            }
-        }
-        Ok(readers)
     }
 
     pub fn unarchive(&self, tar_path: String, dest: String) -> Result<String, Error>{
